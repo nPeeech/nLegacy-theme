7c7ebf359b10afcb1160ef4fc411d8f1d0e27191
nPeeech <33167904+nPeeech@users.noreply.github.com>
2021-06-26 21:56:53 +0900

    訪問者カウンターがゼロパディングされるようにした

--- static/js/header.js
+++ static/js/header.js
@@ -1,9 +1,10 @@
 window.onload = function () {
-    let count = Math.floor(Math.random() * 10000);
-    count = count.toString();
+    const count = Math.floor(Math.random() * 90000);
+    let countStr = "00000000" + count.toString();
+    countStr = countStr.slice(-8);
     let output = "";
-    for (let i = 0; i < count.length; i++) {
-        output += '<span class="frame">' + count[i] + '</span>';
+    for (let i = 0; i < countStr.length; i++) {
+        output += '<span class="frame">' + countStr[i] + '</span>';
     }
     document.querySelector("#counter").innerHTML = output;
 }
